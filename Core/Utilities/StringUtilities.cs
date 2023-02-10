5ca225c7d5bccfe796798c7db63f2b5d44a040dc
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2023-02-10 17:57:37 +0330

    add string and Date Utilities

---
+++ Core/Utilities/StringUtilities.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Core.Utilities
+{
+    public static class StringUtilities
+    {
+        public static string MakeUpperText(string text)
+        {
+            var result = text.ToUpper();
+            return result;
+        }
+
+        public static string MakeLowerText(string text)
+        {
+            var result = text.ToLower();
+            return result;
+        }
+    }
+}
