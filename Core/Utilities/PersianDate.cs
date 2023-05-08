224a81b6403e94a085a112e8231d46fa086e21f0
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2023-05-09 00:01:44 +0430

    add ToGregorian method for PersianDate

---
+++ Core/Utilities/PersianDate.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace Core.Utilities
+{
+    public static class PersianDate
+    {
+
+        public static string ToGregorian(this DateTime date)
+        {
+            GregorianCalendar en = new GregorianCalendar();
+            return string.Format("{0}/{1}/{2}", en.GetYear(date).ToString(), en.GetMonth(date).ToString("00"),
+                en.GetDayOfMonth(date).ToString("00"));
+        }
+
+    }
+}
