8263bcce16aa014f1fcca28e1df78aa9c4fb03e0
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2023-05-09 00:02:26 +0430

    add ToGregorian with tow input

--- Core/Utilities/PersianDate.cs
+++ Core/Utilities/PersianDate.cs
@@ -12,5 +12,14 @@ namespace Core.Utilities
                 en.GetDayOfMonth(date).ToString("00"));
         }
 
+        public static string ToGregorian(this string date, bool isPersian)
+        {
+            int year = int.Parse(date.Substring(0, 4));
+            int month = int.Parse(date.Substring(5, 2));
+            int day = int.Parse(date.Substring(8, 2));
+            PersianCalendar fa = new PersianCalendar();
+            DateTime dt = new DateTime(year, month, day, fa);
+            return dt.ToString("yyyy/MM/dd");
+        }
     }
 }
