b70ee69e679aac8a41af4f6de836dec065ba82ee
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2023-05-08 23:59:31 +0430

    add a Base64Convertor method

---
+++ Core/Utilities/Base64Convertor.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Core.Utilities
+{
+    public static class Base64Convertor
+    {
+        public static string ToBase64(byte[] fileContent)
+        {
+            var base64String = Convert.ToBase64String(fileContent);
+            return $"{base64String}";
+        }
+
+        public static string ToBase64(byte[] fileContent, string contentType)
+        {
+            var base64String = Convert.ToBase64String(fileContent);
+            return $"{contentType};{base64String}";
+        }
+    }
+}
