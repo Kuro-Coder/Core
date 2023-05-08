fc9507522958479acc6603fca13f5446744aeb0f
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2023-05-09 00:03:20 +0430

    add another method for date convert

--- Core/Utilities/PersianDate.cs
+++ Core/Utilities/PersianDate.cs
@@ -21,5 +21,41 @@ namespace Core.Utilities
             DateTime dt = new DateTime(year, month, day, fa);
             return dt.ToString("yyyy/MM/dd");
         }
+
+        public static string ToGregorian(this string date)
+        {
+            date = date.PersianToEnglish();
+            int year = int.Parse(date.Substring(0, 4));
+            int month = int.Parse(date.Substring(5, 2));
+            int day = int.Parse(date.Substring(8, 2));
+            PersianCalendar fa = new PersianCalendar();
+            DateTime dt = new DateTime(year, month, day, fa);
+            return dt.ToString("yyyy/MM/dd");
+        }
+
+        public static string PersianToEnglish(this string persianStr)
+        {
+            Dictionary<char, char> LettersDictionary = new Dictionary<char, char>
+            {
+                ['۰'] = '0',
+                ['۱'] = '1',
+                ['۲'] = '2',
+                ['۳'] = '3',
+                ['۴'] = '4',
+                ['۵'] = '5',
+                ['۶'] = '6',
+                ['۷'] = '7',
+                ['۸'] = '8',
+                ['۹'] = '9'
+            };
+            foreach (var item in persianStr)
+            {
+                if (LettersDictionary.Any(x => x.Key == item))
+                {
+                    persianStr = persianStr.Replace(item, LettersDictionary[item]);
+                }
+            }
+            return persianStr;
+        }
     }
 }
