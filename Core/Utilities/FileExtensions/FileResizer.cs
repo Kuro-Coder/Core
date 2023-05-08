fba1e9a33ca77d7eb1202ed44d8716060563a799
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2023-05-08 23:58:21 +0430

    next step finish FileResizer method

---
+++ Core/Utilities/FileExtensions/FileResizer.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Core.Utilities.FileExtensions
+{
+    public static class FileResizer
+    {
+
+        //using Image image = Image.Thumbnail("0011 - Copy.jpg", 300, 300);
+        //image.WriteToFile("my-thumbnail.jpg");
+
+        //using (Image image = Image.Load("aspose-logo.jpg"))
+        //{
+        //    // Resize image and save the resized image
+        //    image.Resize(300, 300);
+        //    image.Save("SimpleResizing_out.jpg");
+        //}
+    }
+}
